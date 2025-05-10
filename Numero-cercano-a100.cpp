09aeaf5cd2013fc7a2dc5f0a9b589f1c540653db
Racheloviedo <00033125@uca.edu.sv>
2025-05-10 12:32:08 -0600

    Agregando numero cercano a 100

---
+++ Numero-cercano-a100.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int number;
+    int a;
+    int b;
+    cout<<"insert a number for a";
+    cin>>a;
+    cout<<"insert a number for b";
+    cin>>b;
+    if (a<=100 && b>100 )
+    {
+        cout<<"a is near to 100 than b";
+    } 
+    else if (a>100 && b>=100);{
+        cout<<"b is near to 100 than a is";
+
+    }
+    
+
+    return 0;
+}
