a83211852e6da2e0ccd6e398199772e13a48a8ac
Racheloviedo <00033125@uca.edu.sv>
2025-06-24 16:30:03 -0600

    update

---
+++ Ejercicios funciones/suma.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+//Función de Suma
+//Escribe una función que reciba dos números y devuelva su suma.
+
+float suma(float a, float b){
+    return a + b;
+}
+
+int main(){
+    int number1, number2;
+
+    cout << "Please insert the numbers for the addition" << endl;
+    cout << "first number: " << endl;
+    cin >> number1;
+    cout << "second number: " << endl;
+    cin >> number2;
+
+    cout <<"----Result----" << endl;
+    cout << suma(number1, number2) << endl;
+    
+    return 0;
+}
